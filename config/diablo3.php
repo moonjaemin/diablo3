4a2377aef78b64c0d93a6fe484216052fb355bb1
moonjaemin <jjangaboy01@naver.com>
2018-03-14 09:37:03 +0900

    add diablo3 config

---
+++ config/diablo3.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'key1' => env('DIABLO3_API_KEY1'),
+    'secret1' => env('DIABLO3_API_SECRET1'),
+    'key2' => env('DIABLO3_API_KEY2'),
+    'secret2' => env('DIABLO3_API_SECRET2'),
+];
